547a4ff222063c5e2379c267deabdbb076b70864
Tozic Gaming <isameldein2012@gmail.com>
2024-08-01 21:11:57 +0400

    Update ClientBase

--- Rise/dllmain.cpp
+++ Rise/dllmain.cpp
@@ -1,5 +1,4 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
-bool isRunning = true; // isRunning bool for ejection/detachment
+bool isRunning = true;
 
 #include <Windows.h>
 
@@ -28,8 +27,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 
         CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)InitializeClient, hModule, 0, nullptr);
     }
-    if (ul_reason_for_call == DLL_PROCESS_DETACH) {
-    }
-    return TRUE;
+
+    return FALSE;
 }
 
