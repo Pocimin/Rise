12ac943c2fe86a1e8b00cbb822c945a4eacd33a5
FreezeEngine <mr.springtrap@yandex.ru>
2024-08-02 14:56:14 +0300

    Revert "Update ClientBase"
    This reverts commit 547a4ff222063c5e2379c267deabdbb076b70864.

--- Rise/dllmain.cpp
+++ Rise/dllmain.cpp
@@ -1,4 +1,5 @@
-bool isRunning = true;
+// dllmain.cpp : Defines the entry point for the DLL application.
+bool isRunning = true; // isRunning bool for ejection/detachment
 
 #include <Windows.h>
 
@@ -27,7 +28,8 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 
         CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)InitializeClient, hModule, 0, nullptr);
     }
-
-    return FALSE;
+    if (ul_reason_for_call == DLL_PROCESS_DETACH) {
+    }
+    return TRUE;
 }
 
